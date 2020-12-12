f7631ee10182b51fb35ee909008c825823ac0f40
Alex <a.opanasenkov@gmail.com>
2020-12-12 11:14:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// console.log(1);
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// {
+//     let result = 10;
+// }
+// console.log(result);
+
+
+// let number = 5;
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let result = 12 + "3";
+// console.log(result);
+// alert("Hello world!");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваша фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+document.write(answers);
+ 
